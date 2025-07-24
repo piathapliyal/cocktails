6f14c5a31152b17947b6a8a6d63339061a42e90a
Priyanka Thapliyal <priyankathapliyal25@gmail.com>
2025-07-24 13:17:15 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import gsap from 'gsap';
+import { ScrollTrigger, SplitText } from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger, SplitText);
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+      <h1 className='text-3xl text-indigo-300'>hI</h1>
+      </div>
+     
+    </>
+  )
+}
+
+export default App

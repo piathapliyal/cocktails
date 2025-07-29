deb80bf5107cb582745b1a98c55f9f219bc11609
Priyanka Thapliyal <priyankathapliyal25@gmail.com>
2025-07-29 17:14:40 +0530

    cocktail section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import gsap from 'gsap';
 import { ScrollTrigger, SplitText } from "gsap/all";
 import Navbar from './components/Navbar.jsx'
 import Hero from './components/Hero.jsx';
+import Cocktails from './components/Cocktails.jsx';
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 
@@ -13,6 +14,7 @@ function App() {
       <div>
    <Navbar />
     <Hero />
+    <Cocktails />
    
       </div>
      

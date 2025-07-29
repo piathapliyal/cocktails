e71432c9651943f52dbf060e932f751efa683bac
Priyanka Thapliyal <priyankathapliyal25@gmail.com>
2025-07-29 17:45:45 +0530

    menu and contact section

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import Hero from './components/Hero.jsx';
 import Cocktails from './components/Cocktails.jsx';
 import About from './components/About.jsx';
 import Art from './components/Art.jsx';
+import Contact from './components/Contact.jsx';
+import Menu from './components/Menu.jsx';
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 
@@ -19,6 +21,8 @@ function App() {
     <Cocktails />
     <About />
     <Art />
+    <Menu />
+    <Contact />
    
       </div>
      
